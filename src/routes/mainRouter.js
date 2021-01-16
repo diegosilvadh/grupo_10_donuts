f05ac00c9be45ba35cebbf05e3201ad98d2a0abd
diegosilvadh <diegosilvadigitalhouse@gmail.com>
2021-01-16 17:02:27 -0300

    Revert "Revert "Implemetar EJS & Separar Vistas en Carpetas""
    This reverts commit 2688657e625ec9aa601df7221bf083b03f71dcfe.

---
+++ src/routes/mainRouter.js
@@ -0,0 +1,19 @@
+// Modulos y Constantes
+
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+// Rutas de Controller
+
+router.get('/', mainController.index);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/productDetail', mainController.productDetail);
+router.get('/productCart', mainController.productCart);
+router.get('/promotions', mainController.promotions);
+
+
+// Exporta modulo
+
+module.exports = router;
