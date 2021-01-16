77fde1fcb7942876123c8f65fe51645053ff1015
diegosilvadh <diegosilvadigitalhouse@gmail.com>
2021-01-16 19:01:30 -0300

    Create Database

---
+++ src/database/products.js
@@ -0,0 +1,70 @@
+let products = [
+    { 
+        id:1,
+        name: 'Dona Turquesa',
+        price: '$100',
+        discount: '40% Off',
+        images: '/img/dona-turquesa.jpg',
+        description: 'Dona Turquesa',
+      },
+    { 
+        id:2,
+        name: 'Dona Fuxia',
+        price: '$100',
+        discount: '0% Off',
+        images: '/img/dona-fuxia.jpg',
+        description: 'Dona Fuxia',
+      },
+    { 
+      id:3,
+      name: 'Dona Blanca',
+      price: '$100',
+      discount: '0% Off',
+      images: '/img/dona-blanca.jpg',
+      description: 'Dona Blanca',
+    },
+    { 
+        id:4,
+        name: 'Dona Chocolate',
+        price: '$100',
+        discount: '0% Off',
+        images: '/img/dona-choco.jpg',
+        description: 'Dona Chocolate',
+    },
+    { 
+        id:5,
+        name: 'Dona Verde',
+        price: '$150',
+        discount: '30% Off',
+        images: '/img/dona-verde.jpg',
+        description: 'Dona Verde',
+    },
+    { 
+        id:6,
+        name: 'Dona Krispi',
+        price: '$160',
+        discount: '0% Off',
+        images: '/img/ico-donut.jpg',
+        description: 'Dona Dona Krispi',
+    },
+    { 
+        id:7,
+        name: 'Dona Coco',
+        price: '$80',
+        discount: '50% Off',
+        images: '/img/ico-donut.jpg',
+        description: 'Dona Glaseada Coco',
+    },
+    { 
+        id:8,
+        name: 'Dona Choco Krispi',
+        price: '$110',
+        discount: '0% Off',
+        images: '/img/dona-choco-krispi.jpg',
+        description: 'Dona Choco Krispi',
+    },
+
+
+];
+
+module.exports = products;
