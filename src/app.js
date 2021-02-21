cf44e9c56215cb119a44f63df562e6bea1c0b176
diegosilvadh <diegosilvadigitalhouse@gmail.com>
2021-02-21 16:40:38 -0300

    Rutas formulario

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@
 
 const express = require('express');
 const app = express();
-//Definimos el path de las rutas
-
-const mainRouter = require('./routes/mainRouter');
-app.use(express.static('./public'));
+const methodOverride = require('method-override');
 // Template engine
 
+app.use(express.static('./public'));
 app.set('view engine', 'ejs')
 app.set('views', './src/views')
+
+// Formularios
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 // Levantamos el WebServer
 
@@ -17,15 +19,16 @@ app.listen(process.env.PORT || 3000, ()=>{
     console.log('Donas Ready');
 });
 
-//Rutas
+
+//Definimos el path de las rutas
+
+const mainRouter = require('./routes/mainRouter');
+const productsRouter = require('./routes/productsRouter');
+const usersRouter = require('./routes/usersRouter');
 
 app.use('/', mainRouter);
-app.use('/register', mainRouter);
-app.use('/login', mainRouter);
-app.use('/productDetail', mainRouter);
-app.use('/productCart', mainRouter);
-app.use('/promotions', mainRouter);
-app.use('/abmProducts', mainRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 app.use((req,res,next) => {
     res.status(404).render("not-found");
 });
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -7,12 +7,6 @@ const mainController = require('../controllers/mainController');
 // Rutas de Controller
 
 router.get('/', mainController.index);
-router.get('/login', mainController.login);
-router.get('/register', mainController.register);
-router.get('/productDetail', mainController.productDetail);
-router.get('/productCart', mainController.productCart);
-router.get('/promotions', mainController.promotions);
-router.get('/abmProducts', mainController.abmProducts);
 
 // Exporta modulo
 
