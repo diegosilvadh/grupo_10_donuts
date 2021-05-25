d5a006f9e9bbe5869ee768b3e366952bc01133e2
diegosilvadh <diegosilvadigitalhouse@gmail.com>
2021-05-25 12:58:16 -0300

    API User - URL´s

--- src/controllers/api/userController.js
+++ src/controllers/api/userController.js
@@ -8,14 +8,25 @@ const controller = {
         db.User.findAll()
             .then(function(users){
                 const response = {
-                    meta: {
-                        count: users.length,
-                    },
-                    results: users
+                    meta: { count: users.length },
+                    results: users.map(user => {
+                        let userObject = {
+                          id: user.id_user,
+                          first_name: user.first_name,
+                          last_name: user.last_name,
+                          username: user.username,
+                          birthday: user.birthday,
+                          email: user.email,
+                          avatar: `http://localhost:3000/img/users/${user.avatar}`,
+                          id_rol: user.id_rol,
+                          detail: `http://localhost:3000/users/${user.id_user}`
+                        }
+                        return userObject
+                      })
                 }
+                
                 res.status(302).json (response);
             })
-
     },
 
     show: (req,res) => {
@@ -37,9 +48,19 @@ const controller = {
         .then(function(users){
             const response = {
                 meta: {
-                    count: users.length,
+                    count: users.dataValues.id_user.length,
                 },
-                results: users
+                results : {
+                    id: users.dataValues.id_user,
+                    first_name: users.dataValues.first_name,
+                    last_name: users.dataValues.last_name,
+                    username: users.dataValues.username,
+                    birthday: users.dataValues.birthday,
+                    email: users.dataValues.email,
+                    avatar: `http://localhost:3000/img/users/${users.dataValues.avatar}`,
+                    id_rol: users.dataValues.id_rol,
+                    detail: `http://localhost:3000/users/${users.dataValues.id_user}`
+                }
             }
             res.status(302).json (response);
         })
